0be9540b9ad7a5a140587d7d24953e3e71384221
imleijo <leomarjappinen@gmail.com>
2017-09-21 03:13:26 +0300

    Made a Button.js and Style.js and removed all the styles from index.

--- cityzerFront/index.ios.js
+++ cityzerFront/index.ios.js
@@ -4,63 +4,86 @@
  * @flow
  */
 
+/*
+* Commenting inside container requires curly brackets {}
+*/
+
 import React, { Component } from 'react';
+import {Button} from "./Button";
+
 import {
   Image,
   AppRegistry,
-  StyleSheet,
+    StyleSheet,
   Text,
-  View
+  View,
+    TouchableOpacity,
+    Alert,
 } from 'react-native';
 
+
+/*requiring style.js file and defining styles to var s*/
+var s = require('cityzerFront/style.js');
+
 export default class cityzerFront extends Component {
+
+    handleOnPress(){
+        Alert.alert('+1H', 'Sun for a minute', [{text: 'moro'}]);
+    }
+
+    handleOnPress2(){
+        Alert.alert('+2H', 'Its raining', [{text: 'moro'}]);
+    }
+
+    handleOnPress3(){
+        Alert.alert('+3H', 'Black hole', [{text: 'moro'}]);
+    }
+
   render() {
     return (
-      <View style={styles.container}>
+      <View style={s.container}>
 
-
+          {/*Button for location update*/}
+            <Button style={s.location}/>
 
           {/*Address and get location button*/}
-        <Text style={styles.welcome}>
-            <Image
-                style={styles.location}
-                source={{uri:'https://i.imgur.com/K67wWwj.gif'}}
-            />Ratapihantie 13, 00520, 00520 Helsinki
-        </Text>
+          <Text style={s.welcome}>
+             Ratapihantie 13, 00520, 00520 Helsinki
+          </Text>
 
           {/*Timestamp*/}
-        <Text style={styles.timestamp}>
+        <Text style={s.timestamp}>
           12:00
         </Text>
 
           {/*main picture*/}
         <Image
-            style={styles.mainImage}
+            style={s.mainImage}
             source={{uri:'https://i.imgur.com/DmEIa4L.gif'}}
         />
 
           {/*Flex table*/}
           <View style={{flex: 1, flexDirection: 'row'}}>
-              <Text style={styles.infoText}>
+              <Text style={s.infoText}>
                   Celcius{'\n'}
-                  <Text style={styles.info}>
+                  <Text style={s.info}>
                       23°
                   </Text>
               </Text>
 
               {/*infoTex2 because it would automatically add margintop: 10 after implementing image*/}
-              <Text style={styles.infoText2}>
+              <Text style={s.infoText2}>
                   Wind{'\n'}
                   <Image
-                      style={styles.infoImage}
+                      style={s.infoImage}
                       source={{uri:'https://i.imgur.com/A14dO55.gif'}}
                   />
               </Text>
 
-              <Text style={styles.infoText}>
+              <Text style={s.infoText}>
                   Rain{'\n'}
                   {/*infoRain temporary*/}
-                  <Text style={styles.infoRain}>
+                  <Text style={s.infoRain}>
                       0,0{'\n'}
                       mm{'\n'}
                       (1h)
@@ -71,15 +94,21 @@ export default class cityzerFront extends Component {
 
           {/*Button for estimates*/}
           <View style={{flex: 1, flexDirection: 'row'}}>
-              <Text style={styles.heading1}>
-                  1H
-              </Text>
-              <Text style={styles.heading2}>
-                  2H
-              </Text>
-              <Text style={styles.heading3}>
-                  3H
-              </Text>
+              <TouchableOpacity onPress={this.handleOnPress}>
+                  <Text style={s.heading1}>
+                      1H
+                  </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this.handleOnPress2}>
+                  <Text style={s.heading2}>
+                      2H
+                  </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this.handleOnPress}>
+                  <Text style={s.heading3}>
+                      3H
+                  </Text>
+              </TouchableOpacity>
           </View>
 
       </View>
@@ -90,113 +119,6 @@ export default class cityzerFront extends Component {
 
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-      marginTop: 20,
-    alignItems: 'center',
-      justifyContent: 'center',
-    backgroundColor: '#DAECF8',
-  },
-    location: {
-      width: 40,
-        height: 40,
-    },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-    timestamp: {
-      fontSize: 100,
-        marginTop: -20,
-        color: '#FFFFFF',
-        textShadowColor:'#333333',
-        textShadowOffset: {width: 1, height: 1},
-        textShadowRadius: 4,
-    },
-    mainImage: {
-        width: 225,
-        height: 225,
-    },
-    infoText: {
-      fontSize: 25,
-        marginLeft: 10,
-        marginRight: 10,
-        marginTop: 10,
-        marginBottom: -15,
-        padding: 5,
-        textAlign: 'center',
-  },
-    infoText2: {
-        fontSize: 25,
-        marginLeft: 10,
-        marginRight: 10,
-        marginBottom: -15,
-        padding: 5,
-        textAlign: 'center',
-    },
-    info: {
-        fontSize: 35,
-        color: '#ff002f',
-        width: 50,
-        height: 50,
-    },
-    infoRain: {
-      fontSize: 20,
-        color: '#FFFFFF',
-        textShadowColor:'#333333',
-        textShadowOffset: {width: 1, height: 1},
-    },
-    infoImage: {
-      width: 50,
-        height: 50,
-    },
-    heading1: {
-        fontSize: 50,
-        marginTop: 30,
-        borderWidth: 4,
-        borderRadius: 10,
-        marginLeft: 2,
-        marginRight: 2,
-        paddingTop: 10,
-        paddingLeft: 1,
-        paddingRight: 1,
-        overflow: 'hidden',
-        textAlign: 'center',
-        borderColor:'#ffffff',
-        backgroundColor:'#b0e0e6',
-    },
-    heading2: {
-        fontSize: 50,
-        marginTop: 30,
-        borderWidth: 4,
-        borderRadius: 10,
-        marginLeft: 2,
-        marginRight: 2,
-        paddingTop: 10,
-        paddingLeft: 1,
-        paddingRight: 1,
-        overflow: 'hidden',
-        textAlign: 'center',
-        borderColor:'#ffffff',
-        backgroundColor:'#87ceeb',
-    },
-    heading3: {
-        fontSize: 50,
-        marginTop: 30,
-        borderWidth: 4,
-        borderRadius: 10,
-        marginLeft: 2,
-        marginRight: 2,
-        paddingTop: 10,
-        paddingLeft: 1,
-        paddingRight: 1,
-        overflow: 'hidden',
-        textAlign: 'center',
-        borderColor:'#ffffff',
-        backgroundColor:'#4682b4',
-    },
-});
+
 
 AppRegistry.registerComponent('cityzerFront', () => cityzerFront);
