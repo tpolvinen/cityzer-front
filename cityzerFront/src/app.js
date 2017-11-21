8c22382dc1d5befbed6d800c72ea2e568f995c87
Pitkälä Jaakko <a1500949@myy.haaga-helia.fi>
2017-11-21 12:01:57 +0200

    yöllä kuu

--- cityzerFront/src/app.js
+++ cityzerFront/src/app.js
@@ -58,9 +58,18 @@ class App extends Component {
     weatherState(x,y) {
         console.log(x+ ' ' + y);
         let imgSrc = '';
+        var time = new Date().getHours().toLocaleString();
+        time = parseInt(time);
+
+        console.log(time);
+
         if (y > 2) {
             if (x <= 0.3){
-                this.imgSrc = require('./img/sun.png');
+                if (time > 21 && time <= 5){
+                    this.imgSrc = require('./img/moon.png');}
+                else {
+                    this.imgSrc = require('./img/sun.png');
+                }
                 this.bgImg = require('./img/blurbag/blur-backgrounds/blur-backgroundSun_1280x1920.jpg');
                 this.state.buttonStyle = require('./components/sunStyle');
 //Aurinkoinen sää
@@ -89,9 +98,14 @@ class App extends Component {
         }
         else if(y > 0 && y <= 2) {
             if (x <= 0.3) {
-                this.imgSrc = require('./img/sun.png');
+                if (time > 21 && time <= 5){
+                    this.imgSrc = require('./img/moon.png');}
+                else {
+                    this.imgSrc = require('./img/sun.png');
+                }
                 this.bgImg = require('./img/blurbag/blur-backgrounds/blur-backgroundSun_1280x1920.jpg');
                 this.state.buttonStyle = require('./components/sunStyle');
+
 
             } else if (x >= 0.31 && x <= 0.9) {
 
@@ -112,7 +126,11 @@ class App extends Component {
         }
         else{
             if (x <= 0.3) {
-                this.imgSrc = require('./img/sun.png');
+                if (time > 21 && time <= 5){
+                    this.imgSrc = require('./img/moon.png');}
+                else {
+                    this.imgSrc = require('./img/sun.png');
+                }
                 this.bgImg = require('./img/blurbag/blur-backgrounds/blur-backgroundSun_1280x1920.jpg');
 //Aurinkoinen sää
             } else if (x >= 0.31 && x <= 0.9) {
@@ -184,8 +202,8 @@ class App extends Component {
     }
 
     componentDidMount() {
-        this.imgSrc = require('./img/sun.png');
-        this.bgImg = require('./img/blurbag/blur-backgrounds/blur-backgroundSun_1280x1920.jpg');
+        /*this.imgSrc = require('./img/sun.png');
+        this.bgImg = require('./img/blurbag/blur-backgrounds/blur-backgroundSun_1280x1920.jpg');*/
         AppState.addEventListener('change', this._handleAppStateChange);
         navigator.geolocation.getCurrentPosition(
             (position) => {
